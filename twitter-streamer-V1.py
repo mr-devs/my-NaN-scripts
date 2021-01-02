453f5d991a17b57c002a1fa34f57d2a182002364
Matthew R. DeVerna <48068852+mr-devs@users.noreply.github.com>
2021-01-02 17:52:37 -0500

    Removed email update functionality for simplication reasons.

--- twitter-streamer-V1.py
+++ twitter-streamer-V1.py
@@ -14,12 +14,16 @@ OUTPUT:
     - One JSON file is created - per day - where each
     line item represents one tweet object. 
 
-DEPENDENCY:
-    - emailer.py should be kept in the same directory as
-    this file.
+DEPENDENCIES:
+    - Tweepy (https://www.tweepy.org/)
 
 Author: Matthew R. DeVerna
 Date: 12/29/2020
+
+CHANGELOG:
+    - 01/02/2020: Removed the scripts use of emailer.py
+    email updating system functionality to simplify the
+    script.
 
 """
 
@@ -34,8 +38,6 @@ from datetime import datetime as dt
 
 # Dependencies
 from tweepy import OAuthHandler, Stream, StreamListener
-from emailer import eMessages, send_email
-from email.mime.text import MIMEText
 
 
 
@@ -43,7 +45,7 @@ from email.mime.text import MIMEText
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Get current time
-now = dt.strftime(dt.now(), '%m-%d-%Y %H-%M-%S')
+now = dt.strftime(dt.now(), '%m-%d-%Y_%H-%M-%S')
 
 # Define file handler and set logger formatter
 log_filename = f"{now}_stream.log"
@@ -52,7 +54,7 @@ log_filename = f"{now}_stream.log"
 logging.basicConfig(
     filename= log_filename,
     format='%(levelname)s - %(asctime)s | %(message)s',
-    datefmt= "%m-%d-%Y %H-%M-%S",
+    datefmt= "%m-%d-%Y_%H-%M-%S",
     level= logging.INFO)
 
 
@@ -108,22 +110,7 @@ class Listener(StreamListener):
     
     This Listener does the following:
         - Writes raw tweet data to a file named for the day it is scraped.
-        - Provides email updates at the end of each day.
-        - Provides email updates when being rate limited by Twitter.
     """
-    def __init__(
-        self,
-        time,
-        log_filename,
-        total_tweets,
-        todays_tweets,
-        rate_limits,
-        ):
-        self._time = time
-        self._log_filename = log_filename
-        self._total_tweets = total_tweets
-        self._todays_tweets = todays_tweets
-        self._rate_limits = rate_limits
 
     def on_data(self, data):
         today = dt.strftime(dt.now(), "%m-%d-%Y")
@@ -147,35 +134,12 @@ class Listener(StreamListener):
         if os.path.isfile(file_name):
             with open(file_name, "a+") as f:
                 f.write(f"{data}")
-                self._total_tweets += 1
-                self._todays_tweets += 1
-                self._rate_limits = 0
 
         else:
-            logging.info("Sending daily email update...")
-            # Create email update message
-            e_message = eMessages(
-                time = dt.strftime(dt.now(), "%m-%d-%Y_%H-%M-%S"),
-                log_filename = log_filename,
-                total_tweets = self._total_tweets,
-                todays_tweets = self._todays_tweets).daily_update()
-
-            # Send email
-            send_email(
-                email_message = e_message,  # Creates body of email
-                emailType = "daily_update"  # Creates email subject
-                )
-
-            # Set todays tweet count to zero and write data.
-            self.todays_tweets = 0
-
             logging.info(f"Creating file: {file_name}")
 
             with open(file_name, "a+") as f:
                 f.write(f"{data}")
-                self._total_tweets += 1
-                self._todays_tweets += 1
-                self._rate_limits = 0
 
         return True
 
@@ -185,21 +149,6 @@ class Listener(StreamListener):
         # Log error with exception info
         logging.error(f"Error, code {status_code}", exc_info=True) 
         if status_code == 420:
-            self._rate_limits += 1 
-
-            logging.info("Waiting 300 seconds due to rate-limiting.")
-
-            # Create rate limiting email message 
-            e_message = eMessages(
-                time = dt.strftime(dt.now(), "%Y-%m-%d"),
-                log_filename = log_filename,
-                rate_limits = self._rate_limits).rate_limit()
-
-            # Send email
-            send_email(
-                email_message = e_message, # Creates body of email
-                emailType = "rate_limit"   # Creates email subject
-                )
 
             # Wait five minutes
             time.sleep(300)
@@ -235,13 +184,7 @@ if __name__ == '__main__':
 
     # Set up the stream.
     logging.info("Setting up the stream...")
-    listener = Listener(
-        time = dt.strftime(dt.now(), '%m-%d-%Y_%H-%M-%S'),
-        log_filename = log_filename,
-        total_tweets = 0,
-        todays_tweets = 0,
-        rate_limits= 0
-        )
+    listener = Listener()
     auth = OAuthHandler(api_key, api_key_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, listener)
@@ -257,6 +200,3 @@ if __name__ == '__main__':
         except Exception as e:
             logging.debug('Unexpected exception: %s %e')
             continue
-
-
-
